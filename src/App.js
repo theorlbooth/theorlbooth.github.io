47edc558228e9730e3875d6989a6ff0cd1b47338
Theo Booth <theorlbooth@googlemail.com>
2021-01-05 21:00:38 +0000

    Portfolio

--- src/App.js
+++ src/App.js
@@ -3,16 +3,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import './styles/style.scss'
 
-const App = () => (
+import Home from './components/Home'
+
+const App = () => {
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={Home}/>
+      <Route exact path="/" component={Home} />
     </Switch>
   </BrowserRouter>
-)
-
-const Home = () => {
-  return <h1>*** Under Construction ***</h1>
 }
 
 export default App
