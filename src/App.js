f32b68a8c4dad6d892cc97ee6dee3bf40c9f92fd
Theo Booth <theorlbooth@googlemail.com>
2021-01-05 21:01:41 +0000

    Home

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Home from './components/Home'
 const App = () => {
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={Home} />
+      <Route exact path="/home" component={Home} />
     </Switch>
   </BrowserRouter>
 }
