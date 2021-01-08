0c5437463ec299a37b5ed7efbe8a43ad10224b12
Theo Booth <theorlbooth@googlemail.com>
2021-01-08 14:02:10 +0000

    Rotate

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,13 +3,22 @@ import React from 'react'
 import { SiMailDotRu } from 'react-icons/si'
 import { FaLinkedin } from 'react-icons/fa'
 
+import Slide from 'react-reveal/Slide'
+
+
 const Contact = () => {
   return <>
     <div id="contact" style={{ backgroundColor: '#35a79c', padding: '2% 0%' }}>
       <div className="columns is-multiline is-mobile is-centered">
-        <div className="column is-one-fifth-desktop is-3-mobile"><a href="https://www.linkedin.com/in/theorlbooth/"><FaLinkedin /></a></div>
-        <div className="column is-one-fifth-desktop is-3-mobile"><a href="https://github.com/theorlbooth"><i className="devicon-github-original"></i></a></div>
-        <div className="column is-one-fifth-desktop is-3-mobile"><a href="mailto:theorlbooth@googlemail.com"><SiMailDotRu /></a></div>
+        <Slide left>
+          <div className="column is-one-fifth-desktop is-3-mobile"><a href="https://www.linkedin.com/in/theorlbooth/"><FaLinkedin /></a></div>
+        </Slide>
+        <Slide bottom>
+          <div className="column is-one-fifth-desktop is-3-mobile"><a href="https://github.com/theorlbooth"><i className="devicon-github-original"></i></a></div>
+        </Slide>
+        <Slide right>
+          <div className="column is-one-fifth-desktop is-3-mobile"><a href="mailto:theorlbooth@googlemail.com"><SiMailDotRu /></a></div>
+        </Slide>
       </div>
     </div>
   </>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,42 +2,90 @@ import React from 'react'
 
 import { SiMicrosoftexcel, SiFlask } from 'react-icons/si'
 
+import Rotate from 'react-reveal/Rotate'
+
 const Skills = () => {
   return <>
     <div id="skills" style={{ backgroundColor: '#35a79c', paddingBottom: '30px' }}>
       <div className="current">
         <h2>Current Skills</h2>
         <div className="columns is-multiline is-mobile is-centered">
-          <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-babel-plain"></i><div className="name">Babel</div></div>
-          <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-css3-plain"></i><div className="name">CSS3</div></div>
-          <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-express-original"></i><div className="name">Express</div></div>
-          <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-git-plain"></i><div className="name">Git</div></div>
-          <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-github-original"></i><div className="name">GitHub</div></div>
-          <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-heroku-original"></i><div className="name">Heroku</div></div>
-          <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-html5-plain"></i><div className="name">HTML5</div></div>
-          <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-javascript-plain"></i><div className="name">JavaScript</div></div>
-          <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-mongodb-plain"></i><div className="name">MongoDB</div></div>
-          <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-nodejs-plain"></i><div className="name">NodeJS</div></div>
-          <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-npm-original-wordmark"></i><div className="name">npm</div></div>
-          <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-postgresql-plain"></i><div className="name">PostgrSQL</div></div>
-          <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-python-plain"></i><div className="name">Python</div></div>
-          <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-react-original"></i><div className="name">React</div></div>
-          <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-sass-original"></i><div className="name">SASS</div></div>
-          <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-visualstudio-plain"></i><div className="name">VSCode</div></div>
-          <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-webpack-plain"></i><div className="name">Webpack</div></div>
-          <div className="column is-one-fifth-desktop is-one-third-mobile"><SiMicrosoftexcel /><div className="name">Excel</div></div>
-          <div className="column is-one-fifth-desktop is-one-third-mobile"><SiFlask /><div className="name">Flask</div></div>
+          <Rotate top left>
+            <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-babel-plain"></i><div className="name">Babel</div></div>
+          </Rotate>
+          <Rotate top left>
+            <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-css3-plain"></i><div className="name">CSS3</div></div>
+          </Rotate>
+          <Rotate top left>
+            <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-express-original"></i><div className="name">Express</div></div>
+          </Rotate>
+          <Rotate top left>
+            <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-git-plain"></i><div className="name">Git</div></div>
+          </Rotate>
+          <Rotate top left>
+            <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-github-original"></i><div className="name">GitHub</div></div>
+          </Rotate>
+          <Rotate top left>
+            <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-heroku-original"></i><div className="name">Heroku</div></div>
+          </Rotate>
+          <Rotate top left>
+            <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-html5-plain"></i><div className="name">HTML5</div></div>
+          </Rotate>
+          <Rotate top left>
+            <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-javascript-plain"></i><div className="name">JavaScript</div></div>
+          </Rotate>
+          <Rotate top left>
+            <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-mongodb-plain"></i><div className="name">MongoDB</div></div>
+          </Rotate>
+          <Rotate top left>
+            <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-nodejs-plain"></i><div className="name">NodeJS</div></div>
+          </Rotate>
+          <Rotate top left>
+            <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-npm-original-wordmark"></i><div className="name">npm</div></div>
+          </Rotate>
+          <Rotate top left>
+            <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-postgresql-plain"></i><div className="name">PostgrSQL</div></div>
+          </Rotate>
+          <Rotate top left>
+            <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-python-plain"></i><div className="name">Python</div></div>
+          </Rotate>
+          <Rotate top left>
+            <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-react-original"></i><div className="name">React</div></div>
+          </Rotate>
+          <Rotate top left>
+            <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-sass-original"></i><div className="name">SASS</div></div>
+          </Rotate>
+          <Rotate top left>
+            <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-visualstudio-plain"></i><div className="name">VSCode</div></div>
+          </Rotate>
+          <Rotate top left>
+            <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-webpack-plain"></i><div className="name">Webpack</div></div>
+          </Rotate>
+          <Rotate top left>
+            <div className="column is-one-fifth-desktop is-one-third-mobile"><SiMicrosoftexcel /><div className="name">Excel</div></div>
+          </Rotate>
+          <Rotate top left>
+            <div className="column is-one-fifth-desktop is-one-third-mobile"><SiFlask /><div className="name">Flask</div></div>
+          </Rotate>
         </div>
       </div>
       <div className="center"></div>
       <div className="learning">
         <h2>Learning</h2>
         <div className="columns is-multiline is-mobile is-centered">
-          <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-amazonwebservices-original"></i><div className="name">Amazon Web Services</div></div>
-          <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-bootstrap-plain"></i><div className="name">Bootstrap</div></div>
-          <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-csharp-plain"></i><div className="name">C#</div></div>
+          <Rotate top right>
+            <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-amazonwebservices-original"></i><div className="name">Amazon Web Services</div></div>
+          </Rotate>
+          <Rotate top right>
+            <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-bootstrap-plain"></i><div className="name">Bootstrap</div></div>
+          </Rotate>
+          <Rotate top right>
+            <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-csharp-plain"></i><div className="name">C#</div></div>
+          </Rotate>
           {/* <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-mocha-plain"></i><div className="name">Mocha</div></div> */}
-          <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-ruby-plain"></i><div className="name">Ruby</div></div>
+          <Rotate top right>
+            <div className="column is-one-fifth-desktop is-one-third-mobile"><i className="devicon-ruby-plain"></i><div className="name">Ruby</div></div>
+          </Rotate>
         </div>
       </div>
 
