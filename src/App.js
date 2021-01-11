c93fa4f74848e8980f84e01ef60767887c1769c1
Theo Booth <theorlbooth@googlemail.com>
2021-01-11 09:02:42 +0000

    Normalize out

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-import 'normalize.css'
+// import 'normalize.css'
 import './styles/style.scss'
 
 import 'react-alice-carousel/lib/alice-carousel.css'
